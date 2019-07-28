ee42769b182e7f9fc41efe8761b0cb710af71285
Matheus <matheusgt4@hotmail.com>
2019-07-28 11:34:10 -0300

    Fix

--- profile.php
+++ profile.php
@@ -6,8 +6,8 @@ require_once __DIR__."/classes/lol.class.php";
 $loader = new \Twig\Loader\FilesystemLoader('views');
 $twig = new \Twig\Environment($loader);
 
-$region = isset($_GET['region']) ? $_GET['region'] : '';
-$user = isset($_GET['q']) ? $_GET['q'] : '';
+$region = isset($_GET['region']) ? $_GET['region'] : '0';
+$user = isset($_GET['q']) ? $_GET['q'] : 'Rock come ku';
 
 // Get a API KEY fron .env
 try{
